043f384f98b62bfdeef603748ff2e590de8ff0a6
RickRich <68098096+rick4rich@users.noreply.github.com>
2020-09-29 11:29:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 name = input("Give the site you need to scrape: ")
-URL = 'https://mt4guru.com'
 page = requests.get(name)
 
 soup = BeautifulSoup(page.content, 'html.parser')
